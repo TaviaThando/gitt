70283bd3ccd4670ad261ba17fefd6c0c8d4627b3
octmoyo021 <octmoyo021@student.wethinkcode.co.za>
2022-06-21 19:33:56 +0200

    git.py now runs, now need to make sure that it does not print the git message

--- git.py
+++ git.py
@@ -3,17 +3,16 @@ from os import system
 system("git ls-remote > git.txt")
 
 with open('git.txt', 'r') as file:
-    while True:
-        if "fatal" in file.read():
-            print("""THIS ISN'T A GIT REPO
-            TRY:
-            echo "# SOEFKWNGFMD" >> README.md
-            git init
-            git add README.md
-            git commit -m "first commit"
-            git branch -M main
-            git remote add origin git@github.com:TaviaThando/SOEFKWNGFMD.git
-            git push -u origin main""")
-            break
-        else:
-            print("GOOD DAY, THIS IS GITT. HERE TO HELP...")
+
+    while "fatal" in file.read():
+
+        print("""THIS ISN'T A GIT REPO
+        TRY:
+        echo "# SOEFKWNGFMD" >> README.md
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin git@github.com:TaviaThando/SOEFKWNGFMD.git
+        git push -u origin main""")
+
